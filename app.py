13bc377baafe59ea87df9bf719541f9f93064acb
bestorbust <panchamid.btech23@rvu.edu.in>
2026-06-12 13:20:24 +0530

    fix werkzeug production error

--- app.py
+++ app.py
@@ -607,4 +607,4 @@ def handle_feedback(data):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
